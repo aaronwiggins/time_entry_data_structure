c0ed42afd9e6218588c83f65dda0c0d71e78f7b0
Aaron Wiggins <apw7177@gmail.com>
2015-05-18 21:27:27 -0400

    add README and really fix time on project v task

---
+++ README.md
@@ -0,0 +1,12 @@
+#Time Entry Data Structure
+A data structure diagram for developers.
+
+##Description
+It will show the relationship between tables. The diagram shows the relationship
+between the industry, clients, developers, project details, and work groups. I
+used SQLite Browser to help set up live databases.
+
+###To Run...
+* Download [SQLite Browser](https://github.com/sqlitebrowser/sqlitebrowser/releases/tag/v3.5.1)
+* Clone github [repository](https://github.com/aaronwiggins/time_entry_data_structure.git)
+* Open `db.sqlite3` with SQLite Browser
--- migration.rb
+++ migration.rb
@@ -7,19 +7,17 @@ require './developers.rb'
 require './industry.rb'
 require './developer_assignments.rb'
 require './projects.rb'
-require './time_on_projects.rb'
 require './work_groups.rb'
 require './time_on_tasks.rb'
 
-# ClientsMigration.migrate(:up)
-# CommentsOnClientsMigration.migrate(:up)
-# IndustryCommentsMigration.migrate(:up)
-# CommentsOnProjectsMigration.migrate(:up)
-# GroupAssignmentsMigration.migrate(:up)
-# DevelopersMigration.migrate(:up)
-# IndustryMigration.migrate(:up)
-# DeveloperAssignmentsMigration.migrate(:up)
-# ProjectsMigration.migrate(:up)
-# TimeOnProjectsMigration.migrate(:up)
-# WorkGroupsMigration.migrate(:up)
-# TimeOnTasksMigration.migrate(:up)
+ClientsMigration.migrate(:up)
+CommentsOnClientsMigration.migrate(:up)
+IndustryCommentsMigration.migrate(:up)
+CommentsOnProjectsMigration.migrate(:up)
+GroupAssignmentsMigration.migrate(:up)
+DevelopersMigration.migrate(:up)
+IndustryMigration.migrate(:up)
+DeveloperAssignmentsMigration.migrate(:up)
+ProjectsMigration.migrate(:up)
+WorkGroupsMigration.migrate(:up)
+TimeOnTasksMigration.migrate(:up)
